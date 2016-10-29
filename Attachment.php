0b4c43fa52a0a49b1c31d57c9393f41a983ed86e
xutl <79109681@qq.com>
2016-10-29 09:31:25 +0800

    update

---
+++ Attachment.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\attachment;
+
+use yii\base\Object;
+
+class Attachment extends Object
+{
+
+}
