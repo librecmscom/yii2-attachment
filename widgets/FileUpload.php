dac87c19e378df03eb457b892a98f00b96d56c82
xuchao <85825770@qq.com>
2017-02-27 12:03:41 +0800

    update

---
+++ widgets/FileUpload.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\attachment\widgets;
+
+use yii\base\Widget;
+
+/**
+ * Class FileUpload
+ * @package yuncms\attachment\widgets
+ */
+class FileUpload extends Widget
+{
+
+}
