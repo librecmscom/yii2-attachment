54f7dc569757149aa47d9241b9cc06fdf5b0c083
xuchao <85825770@qq.com>
2017-07-14 12:38:16 +0800

    新增微信模块入口

---
+++ wechat/Module.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\attachment\wechat;
+
+class Module extends \yuncms\attachment\Module
+{
+
+}
---
+++ wechat/controllers/UploadController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\attachment\wechat\controllers;
+
+use Yii;
+
+/**
+ * Class UploadController
+ * @package yuncms\attachment\wechat\controllers
+ */
+class UploadController extends \yuncms\attachment\components\UploadController
+{
+
+}
