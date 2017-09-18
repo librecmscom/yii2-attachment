16cd9eafa5aa833cdf221d0b7f8b2e4e3ca79fd7
Peter <xutongle@gmail.com>
2017-09-18 10:32:00 +0800

    修改默认配置

--- migrations/M170916073830Add_defailt_settings.php
+++ migrations/M170916073830Add_defailt_settings.php
@@ -15,17 +15,19 @@ class M170916073830Add_defailt_settings extends Migration
      */
     public function safeUp()
     {
-        Yii::$app->settings->set('storePath', '@root/uploads', 'attachment');
-        Yii::$app->settings->set('storeUrl', '@web/uploads', 'attachment');
+        $this->batchInsert('{{%settings}}', ['type','section','key','value','active','created','modified'], [
+            ['string','attachment','storePath','@root/uploads',1,date('Y-m-d H:i:s'),date('Y-m-d H:i:s')],
+            ['string','attachment','storeUrl','@web/uploads',1,date('Y-m-d H:i:s'),date('Y-m-d H:i:s')],
 
-        Yii::$app->settings->set('imageMaxSize', '2M', 'attachment');
-        Yii::$app->settings->set('imageAllowFiles', 'png,jpg,jpeg,gif,bmp', 'attachment');
+            ['string','attachment','imageMaxSize','2M',1,date('Y-m-d H:i:s'),date('Y-m-d H:i:s')],
+            ['string','attachment','imageAllowFiles','png,jpg,jpeg,gif,bmp',1,date('Y-m-d H:i:s'),date('Y-m-d H:i:s')],
 
-        Yii::$app->settings->set('videoMaxSize', '100M', 'attachment');
-        Yii::$app->settings->set('videoAllowFiles', 'flv,swf,mkv,avi,rm,rmvb,mpeg,mpg,ogg,ogv,mov,wmv,mp4,webm,mp3,wav,mid', 'attachment');
+            ['string','attachment','videoMaxSize','100M',1,date('Y-m-d H:i:s'),date('Y-m-d H:i:s')],
+            ['string','attachment','videoAllowFiles','flv,swf,mkv,avi,rm,rmvb,mpeg,mpg,ogg,ogv,mov,wmv,mp4,webm,mp3,wav,mid',1,date('Y-m-d H:i:s'),date('Y-m-d H:i:s')],
 
-        Yii::$app->settings->set('fileMaxSize', '100M', 'attachment');
-        Yii::$app->settings->set('fileAllowFiles', 'rar,zip,tar,gz,7z,bz2,cab,iso,doc,docx,xls,xlsx,ppt,pptx,pdf,txt,md,xml,xmind', 'attachment');
+            ['string','attachment','fileMaxSize','100M',1,date('Y-m-d H:i:s'),date('Y-m-d H:i:s')],
+            ['string','attachment','fileAllowFiles','rar,zip,tar,gz,7z,bz2,cab,iso,doc,docx,xls,xlsx,ppt,pptx,pdf,txt,md,xml,xmind',1,date('Y-m-d H:i:s'),date('Y-m-d H:i:s')],
+        ]);
     }
 
     /**
