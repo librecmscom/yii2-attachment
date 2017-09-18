60d2432c8f3d130e1b9be90a5b0ac504a77a22d8
Peter <xutongle@gmail.com>
2017-09-18 15:58:17 +0800

    更新 Trait

---
+++ AttachmentTrait.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\attachment;
+
+use Yii;
+
+/**
+ * Trait ModuleTrait
+ * @property-read Module $module
+ * @package yuncms\attachment
+ */
+trait AttachmentTrait
+{
+    /**
+     * 获取附件模块配置
+     * @param string $key
+     * @param null $default
+     * @return bool|mixed|string
+     */
+    public function getSetting($key, $default = null)
+    {
+        $value = Yii::$app->settings->get($key, 'attachment', $default);
+        if ($key == 'storePath' || $key == 'storeUrl') {
+            return Yii::getAlias($value);
+        }
+        return $value;
+    }
+
+    /**
+     * 返回允许上传的最大大小单位 Byte
+     * @return int the max upload size in Byte
+     */
+    public function getMaxUploadByte()
+    {
+        return $this->getMaxUploadSize() * 1024 * 1024;
+    }
+
+    /**
+     * 返回允许上传的最大大小单位 MB
+     * @return int the max upload size in MB
+     */
+    public function getMaxUploadSize()
+    {
+        $maxUpload = (int)(ini_get('upload_max_filesize'));
+        $maxPost = (int)(ini_get('post_max_size'));
+        $memoryLimit = (int)(ini_get('memory_limit'));
+        return min($maxUpload, $maxPost, $memoryLimit);
+    }
+}
