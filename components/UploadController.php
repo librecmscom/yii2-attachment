6efaa80a0710624f89abc7fd5d8c3f6411837aea
Peter <xutongle@gmail.com>
2017-09-29 18:09:48 +0800

    test

--- components/UploadController.php
+++ components/UploadController.php
@@ -7,6 +7,7 @@
 
 namespace yuncms\attachment\components;
 
+use DateTime;
 use Yii;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
@@ -85,74 +86,62 @@ class UploadController extends Controller
         ];
     }
 
-    /**
-     * 显示Jquery Ui上传页面
-     * @param string $callback 回调方法
-     * @param int $fileCount 允许上传的文件数量
-     * @return string
-     */
-    public function actionDialog($callback, $fileCount = 1)
+    public function getAliyun()
     {
-        $this->layout = false;
-        return $this->render('dialog', [
-            'callback' => $callback,
-            'inputName' => 'file',
-            'maxFileCount' => $fileCount,
-            //分片大小
-            'chunkSize' => $this->module->getMaxUploadSize() . 'mb',
-            //最大上传大小
-            'maxFileSize' => $this->module->getMaxUploadSize() . 'mb',
-        ]);
-    }
-
-    /**
-     * 文件上传
-     * @return array|mixed|null
-     * @throws \yii\base\InvalidConfigException
-     */
-    public function actionMultipleUpload()
-    {
-        $uploadedFile = UploadedFile::getInstanceByName($this->inputName);
-        $params = Yii::$app->request->getBodyParams();
-        $filename = $this->module->getUnusedPath($this->tempPath . DIRECTORY_SEPARATOR . $uploadedFile->name);
-        $isUploadComplete = ChunkUploader::process($uploadedFile, $filename);
-        if ($isUploadComplete) {
-            if (Yii::$app->hasModule('attachment')) {
-                $config = [
-                    'maxFiles' => 1,
-                    'extensions' => Yii::$app->getModule('attachment')->imageAllowFiles,
-                    'checkExtensionByMimeType' => true,
-                    'mimeTypes' => 'image/*',
-                    "maxSize" => Yii::$app->getModule('attachment')->getMaxUploadByte(),
-                ];
-
-                $uploader = new Uploader([
-                    'fileField' => $filename,
-                    'config' => $config,
-                ]);
-                $uploader->saveLocal();
-                $res = $uploader->getFileInfo();
-                if ($res['state'] == 'SUCCESS') {
-                    return [
-                        "originalName" => $res['original'],
-                        "name" => $res['title'],
-                        "url" => $res['url'],
-                        "size" => $res['size'],
-                        "type" => $res['type'],
-                        "state" => 'SUCCESS'
-                    ];
-                } else {
-                    return [
-                        "state" => Yii::t('app', 'File save failed'),
-                    ];
-                }
-            } else {
-                return [
-                    'filename' => $filename,
-                    'params' => $params,
-                ];
-            }
+        function gmt_iso8601($time)
+        {
+            $dtStr = date("c", $time);
+            $mydatetime = new DateTime($dtStr);
+            $expiration = $mydatetime->format(DateTime::ISO8601);
+            $pos = strpos($expiration, '+');
+            $expiration = substr($expiration, 0, $pos);
+            return $expiration . "Z";
         }
-        return null;
+
+        $id = '6MKOqxGiGU4AUk44';
+        $key = 'ufu7nS8kS59awNihtjSonMETLI0KLy';
+        $host = 'http://post-test.oss-cn-hangzhou.aliyuncs.com';
+        $callbackUrl = "http://oss-demo.aliyuncs.com:23450";
+
+        $callback_param = array('callbackUrl' => $callbackUrl,
+            'callbackBody' => 'filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}',
+            'callbackBodyType' => "application/x-www-form-urlencoded");
+        $callback_string = json_encode($callback_param);
+
+        $base64_callback_body = base64_encode($callback_string);
+        $now = time();
+        $expire = 30; //设置该policy超时时间是10s. 即这个policy过了这个有效时间，将不能访问
+        $end = $now + $expire;
+        $expiration = gmt_iso8601($end);
+
+        $dir = 'user-dir/';
+
+        //最大文件大小.用户可以自己设置
+        $condition = array(0 => 'content-length-range', 1 => 0, 2 => 1048576000);
+        $conditions[] = $condition;
+
+        //表示用户上传的数据,必须是以$dir开始, 不然上传会失败,这一步不是必须项,只是为了安全起见,防止用户通过policy上传到别人的目录
+        $start = array(0 => 'starts-with', 1 => '$key', 2 => $dir);
+        $conditions[] = $start;
+
+
+        $arr = array('expiration' => $expiration, 'conditions' => $conditions);
+        //echo json_encode($arr);
+        //return;
+        $policy = json_encode($arr);
+        $base64_policy = base64_encode($policy);
+        $string_to_sign = $base64_policy;
+        $signature = base64_encode(hash_hmac('sha1', $string_to_sign, $key, true));
+
+        $response = array();
+        $response['accessid'] = $id;
+        $response['host'] = $host;
+        $response['policy'] = $base64_policy;
+        $response['signature'] = $signature;
+        $response['expire'] = $end;
+        $response['callback'] = $base64_callback_body;
+        //这个参数是设置用户上传指定的前缀
+        $response['dir'] = $dir;
+        echo json_encode($response);
     }
 }
